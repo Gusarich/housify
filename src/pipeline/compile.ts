67e4416accb20567963e8c6766789345bc00e4d2
Gusarich <danielsedovzzz@gmail.com>
2024-07-17 11:14:31 +0300

    `compile.ts` adjustment to prepare for CLI

--- src/pipeline/compile.ts
+++ src/pipeline/compile.ts
@@ -1,7 +1,19 @@
 import { parse } from '../grammar/grammar';
+import { CompilerContext } from '../typing/context';
+import { resolveModule } from '../typing/resolve';
 
 export function compile(src: string) {
+    // Parse
+    console.log('Parsing source code...');
     const moduleAst = parse(src);
 
-    console.log(moduleAst);
+    // Resolve
+    console.log('Resolving module...');
+    const ctx = new CompilerContext();
+    resolveModule(moduleAst, ctx);
+
+    // Write
+    // TODO: Implement this
+
+    console.log('Compilation successful!');
 }
